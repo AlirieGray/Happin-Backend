2010127b300155cb6f54deaf5cd28b71026f9fbb
AlirieGray <caitlin.estrellita@gmail.com>
2018-03-04 17:55:33 -0800

    added lookup events by user id route

--- controllers/events.js
+++ controllers/events.js
@@ -5,23 +5,42 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function(app) {
 
+  // get all events
   app.get('/events', (req, res, next) => {
     Event.find(function(err, events) {
+      if (err) {
+        console.log(err)
+        return res.status(500).send({message: "Could not get all events"})
+      }
       return res.send(events);
     })
   })
 
+  // get an event by ID
   app.get('/events/:id', (req, res, next) => {
     Event.findById(req.params.id).exec(function(err, event) {
       if (err) {
-        return res.status(500).send("Could not fetch event");
+        return res.status(500).send("Could not get this event");
       }
       console.log(event);
       return res.status(200).send(event);
     })
   })
 
+  // get the list of events created by a given user
+  app.get('/users/:userId/events', (req, res) => {
+    console.log("getting all events for this user")
+    User.findById(req.params.userId).exec(function(err, user) {
+      console.log(user);
+      if (err) {
+        console.log("Error: " + err);
+        return res.status(401).send({message: "Could not find user", err});
+      }
+      return res.status(200).send({ events: user.events })
+    })
+  })
 
+  // create a new event
   app.post('/events/new', (req, res) => {
     User.findById(req.body.userId).exec(function(err, user) {
       console.log(user)
@@ -48,6 +67,10 @@ module.exports = function(app) {
           return res.status(500).send({message: "Could not save event", err})
         }
         console.log("Saved new event!")
+
+      }).then(() => {
+        user.events.push(event);
+        user.markModified('events');
         return res.status(200).send(event)
       })
     })
